fd1f70fe99ef6ae7a0fbad1cc904c8ff3e3cf66d
Rick Driessen <rick3ssen@gmail.com>
2015-12-09 17:30:40 +0100

    initial setup

---
+++ Day_4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercise1");
+            Exercise1();
+            Console.WriteLine("Exercise2");
+            Exercise2();
+        }
+
+        static void Exercise1()
+        {
+
+        }
+
+        static void Exercise2()
+        {
+
+        }
+    }
+}
